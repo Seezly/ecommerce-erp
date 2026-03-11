a42066e76c0f863160f9ce9607cf80d10e04153b
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-03-11 15:11:12 -0400

    feat: container component created

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,63 @@
             'alt' => 'Picture\'s description'
         ],
     ];
+
+    $items = [
+        1 => [
+            'image' => 'https://plus.unsplash.com/premium_photo-1686247805255-324dfc4e230a?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'title' => 'Some product',
+            'category' => 'Category',
+            'description' => 'Some description',
+            'stock' => '16',
+            'price' => '2.30',
+            'discountPrice' => '2.00',
+            'discount' => '20'
+        ],
+        2 => [
+            'image' => 'https://plus.unsplash.com/premium_photo-1686247805255-324dfc4e230a?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'title' => 'Some product',
+            'category' => 'Category',
+            'description' => 'Some description',
+            'stock' => '16',
+            'price' => '2.30',
+            'discountPrice' => '2.00',
+            'discount' => '20'
+        ],
+        3 => [
+            'image' => 'https://plus.unsplash.com/premium_photo-1686247805255-324dfc4e230a?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'title' => 'Some product',
+            'category' => 'Category',
+            'description' => 'Some description',
+            'stock' => '16',
+            'price' => '2.30',
+            'discountPrice' => '0.00',
+            'discount' => '0'
+        ],
+        4 => [
+            'image' => 'https://plus.unsplash.com/premium_photo-1686247805255-324dfc4e230a?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'title' => 'Some product',
+            'category' => 'Category',
+            'description' => 'Some description',
+            'stock' => '16',
+            'price' => '2.30',
+            'discountPrice' => '2.00',
+            'discount' => '20'
+        ],
+        5 => [
+            'image' => 'https://plus.unsplash.com/premium_photo-1686247805255-324dfc4e230a?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            'title' => 'Some product',
+            'category' => 'Category',
+            'description' => 'Some description',
+            'stock' => '16',
+            'price' => '2.30',
+            'discountPrice' => '0.00',
+            'discount' => '0'
+        ],
+    ];
 @endphp
 
 <x-layouts::index :title="__('Inicio')">
     <x-slider :slider="$slider"/>
+    <x-container type="container" :items="$items"/>
+    <x-container type="slider" :items="$items"/>
 </x-layouts::index>
