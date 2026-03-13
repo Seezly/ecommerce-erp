d8f66079b4c4aa42b98b3302a17c3374f080e053
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-03-13 12:35:01 -0400

    feat: added filter component and updated layout

---
+++ resources/views/components/filter.blade.php
@@ -0,0 +1,63 @@
+<aside class="w-sm z-1 sticky left-0 top-0 h-screen border-r bg-[#fdfdfc] px-8 py-4">
+	<div class="mb-8">
+		<p class="text-lg font-bold">Filtros</p>
+		<p class="text-xs">Aplica filtros para mejorar tu búsqueda</p>
+	</div>
+	<div class="mb-8 w-full">
+		<p class="text-md mb-4 font-medium">Precios</p>
+		<form action="#" class="flex w-full flex-col gap-2">
+			<label class="flex flex-col gap-2" for="">
+				Rango de precios
+				<input type="range" min="1" max="100">
+				<div class="flex items-center justify-between">
+					<span>Min: $1</span>
+					<span>Price: $</span>
+					<span>Max: $100</span>
+				</div>
+			</label>
+		</form>
+	</div>
+	<div class="mb-8 w-full">
+		<p class="text-md mb-4 font-medium">Antiguedad</p>
+		<form action="#" class="flex w-full flex-col gap-2">
+			<label class="flex items-center justify-start gap-2" for="">
+				<input type="checkbox">
+				nuevo
+			</label>
+			<label class="flex items-center justify-start gap-2" for="">
+				<input type="checkbox">
+				viejo
+			</label>
+		</form>
+	</div>
+	<div class="mb-8 w-full">
+		<p class="text-md mb-4 font-medium">Categorías</p>
+		<form action="#" class="flex w-full flex-col gap-2">
+			<label class="flex items-center justify-start gap-2" for="">
+				<input type="checkbox">
+				juegos
+			</label>
+		</form>
+	</div>
+	<div class="mb-8 w-full">
+		<p class="text-md mb-4 font-medium">Subcategorías</p>
+		<form action="#" class="flex w-full flex-col gap-2">
+			<label class="flex items-center justify-start gap-2" for="">
+				<input type="checkbox">
+				netflix
+			</label>
+			<label class="flex items-center justify-start gap-2" for="">
+				<input type="checkbox">
+				netflix
+			</label>
+			<label class="flex items-center justify-start gap-2" for="">
+				<input type="checkbox">
+				netflix
+			</label>
+			<label class="flex items-center justify-start gap-2" for="">
+				<input type="checkbox">
+				netflix
+			</label>
+		</form>
+	</div>
+</aside>
