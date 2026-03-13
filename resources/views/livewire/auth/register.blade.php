3e1fb443dad571b36ed03cddd4ac413e45d41600
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-03-13 12:35:52 -0400

    fix: updated auth layouts and login/register views

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,67 +1,62 @@
-<x-layouts::auth :title="__('Register')">
-    <div class="flex flex-col gap-6">
-        <x-auth-header :title="__('Create an account')" :description="__('Enter your details below to create your account')" />
+@php
+	$countries = [
+	    1 => [
+	        'name' => 'Venezuela',
+	        'flag' => '...',
+	    ],
+	];
+@endphp
 
-        <!-- Session Status -->
-        <x-auth-session-status class="text-center" :status="session('status')" />
+<x-layouts::auth :title="__('Regístrate')">
+	<div class="flex flex-col gap-6">
+		<x-auth-header :title="__('Crear una cuenta')" :description="__('Ingresa tus datos abajo para crear una cuenta')" />
 
-        <form method="POST" action="{{ route('register.store') }}" class="flex flex-col gap-6">
-            @csrf
-            <!-- Name -->
-            <flux:input
-                name="name"
-                :label="__('Name')"
-                :value="old('name')"
-                type="text"
-                required
-                autofocus
-                autocomplete="name"
-                :placeholder="__('Full name')"
-            />
+		<!-- Session Status -->
+		<x-auth-session-status class="text-center" :status="session('status')" />
 
-            <!-- Email Address -->
-            <flux:input
-                name="email"
-                :label="__('Email address')"
-                :value="old('email')"
-                type="email"
-                required
-                autocomplete="email"
-                placeholder="email@example.com"
-            />
+		<form method="POST" action="{{ route('register.store') }}" class="flex flex-col gap-6">
+			@csrf
+			<!-- Name -->
+			<flux:input name="name" :label="__('Nombre completo')" :value="old('name')" type="text" required autofocus
+				autocomplete="name" :placeholder="__('Nombre Completo')" />
 
-            <!-- Password -->
-            <flux:input
-                name="password"
-                :label="__('Password')"
-                type="password"
-                required
-                autocomplete="new-password"
-                :placeholder="__('Password')"
-                viewable
-            />
+			<!-- Email Address -->
+			<flux:input name="email" :label="__('Correo electrónico')" :value="old('email')" type="email" required
+				autocomplete="email" placeholder="email@ejemplo.com" />
 
-            <!-- Confirm Password -->
-            <flux:input
-                name="password_confirmation"
-                :label="__('Confirm password')"
-                type="password"
-                required
-                autocomplete="new-password"
-                :placeholder="__('Confirm password')"
-                viewable
-            />
+			<!-- Phone Number -->
+			<flux:input name="phone-number" :label="__('Número de teléfono')" :value="old('phone-number')" type="text"
+				required autocomplete="phone-number" placeholder="+58 (424) 541-2882" />
 
-            <div class="flex items-center justify-end">
-                <flux:button type="submit" variant="primary" class="w-full" data-test="register-user-button">
-                    {{ __('Create account') }}
-                </flux:button>
-            </div>
-        </form>
+			<!-- Country -->
+			<flux:select name="country" :label="__('País')" required placeholder="Selecciona tu país">
+				@foreach ($countries as $country)
+					<flux:select.option>
+						<div class="flex items-center gap-2">
+							<flux:icon.shield-check variant="mini" class="text-zinc-400" /> {{ $country['name'] }}
+						</div>
+					</flux:select.option>
+				@endforeach
+			</flux:select>
 
-        <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
-            <span>{{ __('Already have an account?') }}</span>
-            <flux:link :href="route('login')" wire:navigate>{{ __('Log in') }}</flux:link>
-        </div>
-    </div>
+			<!-- Password -->
+			<flux:input name="password" :label="__('Contraseña')" type="password" required autocomplete="new-password"
+				:placeholder="__('Contraseña')" viewable />
+
+			<!-- Confirm Password -->
+			<flux:input name="password_confirmation" :label="__('Confirmar Contraseña')" type="password" required
+				autocomplete="new-password" :placeholder="__('Confirmar Contraseña')" viewable />
+
+			<div class="flex items-center justify-end">
+				<flux:button type="submit" variant="primary" class="w-full" data-test="register-user-button">
+					{{ __('Crear cuenta') }}
+				</flux:button>
+			</div>
+		</form>
+
+		<div class="space-x-1 text-center text-sm text-zinc-600 rtl:space-x-reverse dark:text-zinc-400">
+			<span>{{ __('¿Ya tienes una cuenta?') }}</span>
+			<flux:link :href="route('login')" wire:navigate>{{ __('Inicia sesión') }}</flux:link>
+		</div>
+	</div>
 </x-layouts::auth>
