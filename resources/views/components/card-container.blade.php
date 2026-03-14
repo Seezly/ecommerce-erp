9b8a0eb3283eb1fd9c0c7fc97997ceb2e5b4997a
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-03-13 20:26:53 -0400

    refactor: container component refactored into card-slider and card-container components

---
+++ resources/views/components/card-container.blade.php
@@ -0,0 +1,14 @@
+@props([
+    'cols' => '4',
+])
+
+<section
+	{{ $attributes->class([
+	    'mb-8 grid w-full gap-4 p-4',
+	    'grid-cols-3' => $cols == 3,
+	    'grid-cols-4' => $cols == 4,
+	    'grid-cols-5' => $cols == 5,
+	    'grid-cols-6' => $cols == 6,
+	]) }}>
+	{{ $slot }}
+</section>
