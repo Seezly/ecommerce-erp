5207e1d66b69d83e3c41c3d4507c7b3f12614f5a
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-03-19 11:01:52 -0400

    feat: routes updated, single product view added

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 Route::view('/products', 'products')->name('products');
+Route::view('/product/{product}', 'product')->name('product');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
-});
+Route::view('/dashboard', 'dashboard')->name('dashboard');
+Route::view('/favorites', 'favorites')->name('favorites');
+Route::view('/accounts', 'accounts')->name('accounts');
+Route::view('/orders', 'orders')->name('orders');
+Route::view('/user-info', 'user-info')->name('user-info');
+Route::view('/user-security', 'user-security')->name('user-security');
+
+Route::view('/admin', 'admin.index')->name('admin');
 
 require __DIR__ . '/settings.php';
